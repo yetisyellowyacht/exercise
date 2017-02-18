146044896cdfc62497969460385e63d00daab630
yetisyellowyacht <mugofpunch@gmail.com>
2017-02-18 21:27:45 +0600

    continued

--- index.php
+++ index.php
@@ -9,6 +9,9 @@ and open the template in the editor.
 	<head>
 		<meta charset="utf-8">
 		<title>blog-lite</title>
+		<link rel="stylesheet" href="style.css">
+		<link rel="stylesheet" 
+			href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
 	</head>
 	
 	<header>
@@ -17,25 +20,34 @@ and open the template in the editor.
 	</header>
 
 	<body>
+        <div class="container">
 
-        <?php
-        // put your code here
-        ?>
+			<div class="post">
+				<h2>Post 1</h2>
+				<em><?php echo date('Y-m-d, l');?></em>
+				<p>
+					The required format parameter of the date() function specifies 
+					how to format the date (or time). <br>
+					Here are some characters that are commonly used for dates:<br>
+				    d - Represents the day of the month (01 to 31)<br>
+				    m - Represents a month (01 to 12)<br>
+				    Y - Represents a year (in four digits)<br>
+				    l (lowercase 'L') - Represents the day of the week<br>
+					Other characters, like"/", ".", or "-" can also be inserted 
+					between the characters to add additional formatting.
+					The example below formats today's date in three different ways:
+				</p>
+			</div>
 
-		<div>
-			<h2>Post 1</h2>
-			<em><?php echo date('Y-m-d, l');?></em>
-			<p>
-				The required format parameter of the date() function specifies how to format the date (or time). <br>
-				Here are some characters that are commonly used for dates:<br>
-			    d - Represents the day of the month (01 to 31)<br>
-			    m - Represents a month (01 to 12)<br>
-			    Y - Represents a year (in four digits)<br>
-			    l (lowercase 'L') - Represents the day of the week<br>
-				Other characters, like"/", ".", or "-" can also be inserted between the characters to add additional formatting.
-				The example below formats today's date in three different ways:
-			</p>
-		</div>
+			<div class="post">
+				<h2>Post 2</h2>
+				<em><?php echo date('Y-m-d, l');?></em>
+				<p>
+					The required format parameter of the date() function specifies how to format the date (or time). <br>
+					Here are some characters that are commonly used for dates:<br>
+				</p>
+			</div>
+        </div>
 	</body>
 	
 	<footer>
