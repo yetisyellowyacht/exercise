2e905737f8ddd483fcdaa0a357d1060e02ac6092
yetisyellowyacht <mugofpunch@gmail.com>
2017-02-25 17:08:03 +0300

    some optimization.

--- models/type_article.php
+++ models/type_article.php
@@ -1,17 +1,4 @@
 <?php
-	$postsdb = array(
-		array(
-			'id' => '1',
-			'title' => 'Title1',
-			'date' => '2017-02-24',
-			'content' => 'Content1'),
-		array(
-			'id' => '2',
-			'title' => 'Title2',
-			'date' => '2017-02-25',
-			'content' => 'Content2')
-		);
-
 	// Functions to process posts
 	function article_list() {
 		return $GLOBALS['postsdb'];
