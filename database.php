c1aef18d3a6de84fe97a3fd736c8f92f6cff8452
yetisyellowyacht <mugofpunch@gmail.com>
2017-02-25 17:24:13 +0300

    edited text, added 3rd post.

--- database.php
+++ database.php
@@ -3,13 +3,18 @@
 	$postsdb = array(
 		array(
 			'id' => '1',
-			'title' => 'Title1',
+			'title' => 'Basic Rules for Dungeons & Dragons',
 			'date' => '2017-02-24',
-			'content' => 'Content1'),
+			'content' => 'The Basic Rules runs from levels 1 to 20 and covers the cleric, fighter, rogue, and wizard, presenting what we view as the essential subclass for each. It also provides the dwarf, elf, halfling, and human as race options; in addition, the rules contain 120 spells, 5 backgrounds, and character sheets. But the best part? The Basic Rules is a free PDF. Anyone can download it from our website. We want to put D&D in as many hands as possible, and a free, digital file is the best way to do that.'),
 		array(
 			'id' => '2',
-			'title' => 'Title2',
+			'title' => 'What Are Basic English Grammar Rules?',
 			'date' => '2017-02-25',
-			'content' => 'Content2')
+			'content' => 'There are hundreds of grammar rules but the basic ones refer to sentence structure and parts of speech, which are noun, pronoun, verb, adjective, adverb, preposition and conjunction.  Let’s look at the way sentences are put together and the words that form them.'),
+		array(
+			'id' => '3',
+			'title' => 'Rules of Hospitality',
+			'date' => '2017-02-26',
+			'content' => 'Simply explained, the 10 and 5 Rule suggests that anytime a guest is within ten feet of a staff member, the staff member should make eye contact and warmly smile to acknowledge the oncoming guests. <br>When a staff member is approximately five feet from a guest, a sincere greeting or friendly gesture of acknowledgement should accompany the eye contact and smile. <br>Successful companies in and out of the hospitality industry have adopted their own versions of the 10 and 5 Staff Rule. <br>At Wal-Mart, founder Sam Walton coined the \'Ten-Foot Attitude\' and said, "…I want you to promise that whenever you come within 10 feet of a customer, you will look him in the eye, greet him and ask him if you can help him."')
 	);
 ?>
